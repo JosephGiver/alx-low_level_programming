9d445ffbd134f6c6c2a43ca5e9f55b64da5c6619
JosephGiver <josephgiver@gmail.com>
2022-09-11 14:47:52 -0700

    A C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
-* main - prints and that piece of art is useful - Dora Korpar, 2015-10-19
-* followed by a new line.
+* main - prints "and that piece of art is useful"
+* - Dora Korpar, 2015-10-19"
+* followed by a new line, to the standard error.
 * Return: 0
 */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
