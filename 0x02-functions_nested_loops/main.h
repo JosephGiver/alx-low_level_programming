3cc7fff51474599cbe8c4405cc5cde027462272b
JosephGiver <josephgiver@gmail.com>
2022-09-21 16:15:55 +0100

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H_INCLUDED
+define HOLBERTO[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D# [Ddefine HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void)
+int _islowe(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int a, int b);
+void print_to_98(int n);
+void num(int r, int n);
+
+#endif
